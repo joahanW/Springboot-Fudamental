eff613c66ea2a889b7f6e2eeb49008f4f78b2ade
joahan28 <jwirasugianto01@student.ciputra.ac.id>
2021-11-29 22:04:48 +0800

    Spring Initial

---
+++ src/main/java/co/id/mii/springfundamental/SpringFundamentalApplication.java
@@ -0,0 +1,13 @@
+package co.id.mii.springfundamental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFundamentalApplication.class, args);
+	}
+
+}
